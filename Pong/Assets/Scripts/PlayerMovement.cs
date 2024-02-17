2d242716efcb375e5e4a3b8fb81b9626c491163f
Filipe Eduardo <filipeeacosta@gmail.com>
2024-02-17 20:01:06 -0300

    Player Movement
    Add movement and hitboxes for both players

---
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    [SerializeField]
+    public KeyCode KeyCodeUp;
+    [SerializeField]
+    public KeyCode KeyCodeDown;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCodeUp))
+        {
+            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCodeDown))
+        {
+            transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
+        }
+    }
+}

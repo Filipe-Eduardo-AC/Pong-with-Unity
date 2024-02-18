79343edfc3bf3664d6f64ef2874eae4ab2a181fa
Filipe Eduardo <filipeeacosta@gmail.com>
2024-02-17 23:29:42 -0300

    Complete Ball Movement
    Compete Ball Movement function with some Collider adaptation.

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class BallMovement : MonoBehaviour
 {
-    public float speed = 30;
+    [SerializeField]
+    private float speed = 10;
+    public Rigidbody2D rb;
 
-    void Start()
+    private void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        LaunchBall();
     }
 
+    private void LaunchBall()
+    {
+        float randomDirectionX = Random.Range(0, 2) == 0 ? -1 : 1;
+        float randomDirectionY = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(speed * randomDirectionX, speed * randomDirectionY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            float relativePosition = transform.position.y - collision.transform.position.y;
+            float verticalDirection = Mathf.Sign(relativePosition);
+            float horizontalDirection = Mathf.Sign(rb.velocity.x);
+
+            rb.velocity = new Vector2(horizontalDirection * speed * 0.8f, verticalDirection * speed * 0.8f);
+        }
+        else
+        {
+            Vector2 normal = collision.GetContact(0).normal;
+            rb.velocity = Vector2.Reflect(rb.velocity, normal).normalized * speed * 1.2f;
+        }
+    }
 }
